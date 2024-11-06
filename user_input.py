c477ff4520a465dc51930ff01a368ebd768b7c82
vinnietito <vincentnjuguna69@gmail.com>
2024-11-06 08:42:10 +0300

    Commits

--- user_input.py
+++ user_input.py
@@ -1,5 +1,5 @@
 name = input("Please enter your name: ")
 age = input("Please enter your age: ")
-location = input("please enter your location")
+location = input("please enter your location: ")
 
 print(f"Hello {name}, you are {age} years old and live in {location}.")
